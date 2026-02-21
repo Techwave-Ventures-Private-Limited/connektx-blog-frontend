a2f509665bfefa0849cf0ce3478fd3a282b95f62
akash-0963 <akashjare09@gmail.com>
2026-02-22 02:15:09 +0530

    updated logo

--- app/page.js
+++ app/page.js
@@ -36,7 +36,7 @@ export default function Home() {
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="flex justify-between items-center h-20">
               <div className="flex items-center gap-2 cursor-pointer">
-                <img src="public/logo.png" alt="Connektx Logo" className="h-10 w-auto" />
+                <img src="/images/logo.png" alt="Connektx Logo" className="h-16 w-auto" />
                 <span className="font-bold text-xl tracking-tight">Connektx</span>
               </div>
 
