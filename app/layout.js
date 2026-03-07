d440c04f306617788d1b7cf17376ddadf9ac437d
akash-0963 <akashjare09@gmail.com>
2026-03-07 11:26:30 +0530

    Pixel ID change

--- app/layout.js
+++ app/layout.js
@@ -70,7 +70,7 @@ export default function RootLayout({ children }) {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '439392249067743'); 
+            fbq('init', '1644750570206674'); 
             fbq('track', 'PageView');
           `}
         </Script>
